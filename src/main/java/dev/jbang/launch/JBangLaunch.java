f02ace203c8981dcc285cd4fe526164c7c39d9cb
Max Rydahl Andersen <max@xam.dk>
2025-08-05 01:21:14 +0200

    fix: only register handler if supported

--- src/main/java/dev/jbang/launch/JBangLaunch.java
+++ src/main/java/dev/jbang/launch/JBangLaunch.java
@@ -10,37 +10,39 @@ import javax.swing.JOptionPane;
 
 public class JBangLaunch {
     public static void main(String[] args) {
-        if(Desktop.isDesktopSupported()) {
-            Desktop.getDesktop().setOpenURIHandler((event) -> {
-                handleURI(event.getURI());
-            });
+        if (Desktop.isDesktopSupported()) {
+            Desktop dt = Desktop.getDesktop();
+            if (dt.isSupported(Desktop.Action.APP_OPEN_URI)) {
+                dt.setOpenURIHandler((event) -> {
+                    handleURI(event.getURI());
+                });
+            }
         }
 
         if (args != null && args.length > 0) {
             String firstArg = args[0];
-            
+
             URI uri = URI.create(firstArg);
-            
-            if(uri!=null && uri.getScheme()!=null) {
+
+            if (uri != null && uri.getScheme() != null) {
                 handleURI(uri);
                 System.exit(0);
             }
         }
     }
-    
+
     private static void handleURI(URI uri) {
-        
-        if(GraphicsEnvironment.isHeadless()) {
+
+        if (GraphicsEnvironment.isHeadless()) {
             System.out.println(uri);
             return;
         } else {
             JOptionPane.showMessageDialog(
-            null,
-            "Received URL:\n" + uri.toString(),
-            "URL Handler",
-            JOptionPane.INFORMATION_MESSAGE
-            );
+                    null,
+                    "Received URL:\n" + uri.toString(),
+                    "URL Handler",
+                    JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    
+
 }
