f90a407adc5aa0050fac7829f5e29e534781bdd3
Max Rydahl Andersen <max@xam.dk>
2025-08-05 10:06:37 +0200

    fix: use flatlaf to not suck too much

--- src/main/java/dev/jbang/launch/JBangLaunch.java
+++ src/main/java/dev/jbang/launch/JBangLaunch.java
@@ -7,17 +7,14 @@ import java.awt.GraphicsEnvironment;
 import java.net.URI;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class JBangLaunch {
     public static void main(String[] args) {
-        if (Desktop.isDesktopSupported()) {
-            Desktop dt = Desktop.getDesktop();
-            if (dt.isSupported(Desktop.Action.APP_OPEN_URI)) {
-                dt.setOpenURIHandler((event) -> {
-                    handleURI(event.getURI());
-                });
-            }
-        }
+        installURIListener();
 
         if (args != null && args.length > 0) {
             String firstArg = args[0];
@@ -31,12 +28,32 @@ public class JBangLaunch {
         }
     }
 
-    private static void handleURI(URI uri) {
+    /**
+     * Installs the URI listener which is used to handle the jbang:// protocol 
+     * on MacOS.
+     */
+    private static void installURIListener() {
+        if (Desktop.isDesktopSupported()) {
+            Desktop dt = Desktop.getDesktop();
+            if (dt.isSupported(Desktop.Action.APP_OPEN_URI)) {
+                dt.setOpenURIHandler((event) -> {
+                    handleURI(event.getURI());
+                });
+            }
+        }
+    }
 
+    private static void handleURI(URI uri) {
         if (GraphicsEnvironment.isHeadless()) {
             System.out.println(uri);
             return;
         } else {
+             try {
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            } catch (UnsupportedLookAndFeelException e) {
+                System.err.println("WARN: Failed to set look and feel");
+                e.printStackTrace();
+            }
             JOptionPane.showMessageDialog(
                     null,
                     "Received URL:\n" + uri.toString(),
